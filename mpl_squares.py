b18f9397515447df4b40fdfe09304067b71c63e4
Matthew Oyales <moyales@stevens.edu>
2019-12-20 02:46:12 -0500

    First commit

---
+++ mpl_squares.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+plt.plot(squares)
+plt.show()
+
+# A representation of the simplest sort of plot you can make using Matplotlib
+# Plot a set of values and show it
