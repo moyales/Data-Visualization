1a238c62f19bf63da38d3d0ba22308da28e9ea37
Matthew Oyales <moyales@stevens.edu>
2019-12-22 23:56:36 -0500

    Python Repos

--- Demos/Ch17/python_repos.py
+++ Demos/Ch17/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -11,23 +13,61 @@ print("Total repositories:", response_dict['total_count'])
 
 # Explore info about the repositories.
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
+
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+
+    # Get project description, if one is available.
+    description = repo_dict['description']
+    if not description:
+        description = "No description provided."
+    
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': description,
+        'xlink': repo_dict['html_url']
+    }
+    plot_dicts.append(plot_dict)
+
+# Make Visualization
+my_style = LS('#333366', base_style=LCS)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 18
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = "Most Starred Python Projects on GitHub"
+chart.x_labels = names
+
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
+
+
+#print("Repositories returned:", len(repo_dicts))
 
 # Examine the first repository.
-repo_dict = repo_dicts[0]
-print("\nKeys:", len(repo_dict))
-for key in sorted(repo_dict.keys()):
-    print(key)
+#repo_dict = repo_dicts[0]
+#print("\nKeys:", len(repo_dict))
+#for key in sorted(repo_dict.keys()):
+    #print(key)
 
-print("\nSelected info about the each repository:")
-for repo_dict in repo_dicts:
-    print('Name:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
+#print("\nSelected info about the each repository:")
+#for repo_dict in repo_dicts:
+    #print('Name:', repo_dict['name'])
+    #print('Owner:', repo_dict['owner']['login'])
+    #print('Stars:', repo_dict['stargazers_count'])
+    #print('Repository:', repo_dict['html_url'])
     #print('Created:', repo_dict['created_at'])
     #print('Updated:', repo_dict['updated_at'])
-    print('Description:', repo_dict['description'], "\n")
+    #print('Description:', repo_dict['description'], "\n")
 
 # Process results.
-print(response_dict.keys())
+#print(response_dict.keys())
